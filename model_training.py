b6f3186f68d6596e6001de88ac03310dac01ef50
sivasdp <sdpsiva191@gmail.com>
2024-03-15 20:38:55 +0530

    data preprocessing

--- model_training.py
+++ model_training.py
@@ -1,3 +1,17 @@
-from main import say_hello
+import tensorflow
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator 
 
-say_hello()
+dataset_path='dataset/' #make label to solve this error
+
+train_gen=ImageDataGenerator(rescale=1./255,
+                             shear_range=0.2,
+                             zoom_range=0.2,
+                             horizontal_flip=True)
+
+train_data=train_gen.flow_from_directory(dataset_path,target_size=(224,224),batch_size=32)
+
+if train_data:
+    print("Data Created Sucessfully")
+else:
+    print("Some error occured")
